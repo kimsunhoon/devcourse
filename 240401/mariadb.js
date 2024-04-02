921002b1e6ff758a2d8b8a254578b867f67cf6fd
kimsunhoon <brycekim31@gmail.com>
2024-04-02 16:51:54 +0900

    db 모듈 생성

---
+++ 240401/mariadb.js
@@ -0,0 +1,13 @@
+// Get the client
+const mysql = require('mysql2');
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'Channel',
+  dateStrings: true,
+});
+
+module.exports = connection;
